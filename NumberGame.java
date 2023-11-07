2400e339e17194d252f0e5a908a51b6e288f26ee
chenna <chennabasava15@gmail.com>
2023-11-07 17:16:05 +0530

    number game code

--- NumberGame.java
+++ NumberGame.java
@@ -1,5 +1,5 @@
 package PROJECTS;
-import java.util.Scanner;
+import java.util.*;
 import java.util.Random;
 
 public class NumberGame {
@@ -8,28 +8,28 @@ public class NumberGame {
         Random random = new Random();
 
         int maxRange = 100;
-        int maxAttempts = 10;
+        int maxAttempts = 5;
         int rounds = 0;
         int score = 0;
 
-        System.out.println("Welcome to the Number Guessing Game!");
-        boolean playAgain = true;
+        System.out.println("Welcome to the Number Guessing Game");
+        boolean tryAgain = true;
 
-        while (playAgain) {
-            int targetNumber = random.nextInt(maxRange) + 1;
+        while (tryAgain) {
+            int generatedNumber = random.nextInt(maxRange) + 1;
             int attempts = 0;
             int roundScore = 0;
             boolean roundWon = false;
 
-            System.out.println("Round " + (rounds + 1));
+            System.out.println("Round Number: " + (rounds + 1));
 
             while (attempts < maxAttempts) {
                 System.out.print("Guess the number (between 1 and " + maxRange + "): ");
                 int userGuess = scanner.nextInt();
 
-                if (userGuess < targetNumber) {
+                if (userGuess < generatedNumber) {
                     System.out.println("Too low. Try again.");
-                } else if (userGuess > targetNumber) {
+                } else if (userGuess > generatedNumber) {
                     System.out.println("Too high. Try again.");
                 } else {
                     System.out.println("Congratulations! You guessed the correct number.");
@@ -41,7 +41,7 @@ public class NumberGame {
             }
 
             if (!roundWon) {
-                System.out.println("Out of attempts. The correct number was " + targetNumber + ".");
+                System.out.println("Out of attempts. The correct number was " + generatedNumber + ".");
             }
 
             score += roundScore;
@@ -50,7 +50,7 @@ public class NumberGame {
             System.out.println("Your score for this round: " + roundScore);
             System.out.print("Do you want to play another round? (yes/no): ");
             String playAgainInput = scanner.next().toLowerCase();
-            playAgain = playAgainInput.equals("yes");
+            tryAgain = playAgainInput.equals("yes");
         }
 
         System.out.println("Game over! You played " + rounds + " round(s) and your final score is: " + score);
